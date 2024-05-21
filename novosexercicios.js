73b4ec6cf23bd60a798bf9dda394b931bd6dc927
Camila Sayuri <camilayoshioka@gmail.com>
2024-05-20 23:17:00 -0300

    feat: new inputs and counter

--- novosexercicios.js
+++ novosexercicios.js
@@ -24,7 +24,7 @@ let form = document.querySelector('#form1');
 let inpText = document.querySelector('#inpText');
 let botaoEnviar = document.querySelector('#enviar');
 let textao = document.querySelector('#textArea')
-enviar.addEventListener('click', consertaerro, false);
+let botao = enviar.addEventListener('click', submitForm, false);
 
 let lengthArea = document.querySelector('#lengthArea');
 
@@ -35,14 +35,15 @@ function validateTextao() {
 }
 
 
-function consertaerro() {
+/*function consertaerro(botao) {
     if (botaoEnviar && inpText.value=='') {
         alert('Preencha o campo');
     } else {
         alert('Enviado');
     }
-}        
-
+}
+consertaerro(botao);
+*/
 
 inpText.style.backgroundColor = '#FFF9D0';
 
@@ -65,3 +66,107 @@ function countArea(event)
 }
 
 textao.addEventListener('keyup', countArea, false)
+
+
+//Aula 26
+
+let checkbox = document.querySelector('#check');
+console.log(checkbox.checked);
+/* if (checkbox.checked == false){
+    alert('Aceite os termos');
+}
+*/
+
+//Radio
+
+let radio = document.getElementsByName('gender');
+
+function radioTeste(event) {
+    if(event.target.value == 'Male') {
+        alert('Male');
+    } else {
+        alert('Female');
+    }
+}
+for(let i=0; i < radio.length; i++) {
+    radio[i].addEventListener('click', radioTeste, false);
+}
+
+//Select
+
+let sel = document.querySelector('#sel');
+
+function selValidate(event) {
+    if (event.target.selectedIndex == 0) {
+        alert('Selecione uma opção');
+    }
+}
+
+sel.addEventListener('change', selValidate, false);
+
+//Botao
+
+let btn = document.querySelector('#btn');
+
+function submitForm(event) {
+    event.preventDefault();
+}
+
+btn.addEventListener('click', submitForm, false);
+
+//Aula 27
+
+let gallery = document.querySelector('.gallery');
+let files = document.querySelector('#img');
+
+function imgPreview(event) {
+    gallery.innerHTML = '';
+    let files = event.target.files;
+    for (let i=0; i < files.length; i++) {
+        if (files[i].size > 20000) {
+            alert('imagem muito grande')
+        } else {
+            let reader = new FileReader();
+            reader.onload = function (event) {
+                let urlImg = event.target.result;
+                let newImg = document.createElement('img');
+                newImg.setAttribute('src', urlImg);
+                newImg.style = 'width: 100px; margin: 5px;';
+                gallery.appendChild(newImg);
+            }
+        }    
+        reader.readAsDataURL(files[i]);
+    }
+}
+
+files.addEventListener('change', imgPreview, false);
+
+//Aula 28
+
+let novoBotao = document.querySelector('#novobotao');
+
+/*setTimeout(contador, 3000);
+setInterval(contador, 3000);
+console.log('Hello');
+
+function contador() {
+    for (let i=0; i < 100; i++){
+        console.log(i);
+    }
+}
+
+novoBotao.addEventListener('click', contador, false);
+console.log('world');
+*/ 
+
+let i=0;
+function contador() {
+    i++;
+    console.log(i);
+    if(i >= 5){
+        return;
+    }
+    setTimeout(contador, 2000);
+}
+contador();
+
